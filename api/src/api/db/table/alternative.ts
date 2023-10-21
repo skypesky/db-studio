f5347f8c6895f1d9bc48c4d96402bb623f2beba2
skypesky <2565978507@qq.com>
2023-10-21 10:55:52 +0800

    feat: get single table's all data

--- api/src/api/db/table/alternative.ts
+++ api/src/api/db/table/alternative.ts
@@ -1,6 +1,10 @@
 import type { Request, Response } from 'express';
 import type { Knex } from 'knex';
 
-export default function alternative(req: Request & { instance: Knex }, res: Response) {
-  return res.send({ ok: !req });
+export default async function alternative(req: Request & { instance: Knex }, res: Response) {
+  const { name }: { name: string } = req.query as { name: string };
+
+  const table = await req.instance(name).select();
+
+  return res.send(table);
 }
