b9ce50d5dddcf1c6278aeffe6359080057617f1e
skypesky <2565978507@qq.com>
2023-10-22 10:25:26 +0800

    feat: add default api

--- api/src/api/db/table/alternative.ts
+++ api/src/api/db/table/alternative.ts
@@ -4,7 +4,8 @@ import type { Knex } from 'knex';
 export default async function alternative(req: Request & { instance: Knex }, res: Response) {
   const { name }: { name: string } = req.query as { name: string };
 
-  const table = await req.instance(name).select();
+  const data = await req.instance(name).select();
+  const columns = await req.instance(name).withSchema('');
 
-  return res.send(table);
+  return res.send({ data, columns });
 }
